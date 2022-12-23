7c4740265b75af87d02634933703262f8479a7ba
dmike16 <cipmiky@gmail.com>
2022-12-24 00:26:27 +0100

    fix: correct handler func signature

--- pkg/webhook/net/readyz.go
+++ pkg/webhook/net/readyz.go
@@ -7,6 +7,6 @@ import (
 
 // Readyz handle function to implement readiness probe
 //
-func Readyz(w http.ResponseWriter, r http.Request) {
+func Readyz(w http.ResponseWriter, r *http.Request) {
 	_, _ = io.WriteString(w, "ok")
 }
